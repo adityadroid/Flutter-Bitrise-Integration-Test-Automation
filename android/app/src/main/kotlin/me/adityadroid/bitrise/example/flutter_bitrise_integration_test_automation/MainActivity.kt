a237594533298fb5511e05498d16d6ca72411ac3
Aditya Gurjar <17155247+adityadroid@users.noreply.github.com>
2023-04-19 10:23:57 +0530

    first commit

---
+++ android/app/src/main/kotlin/me/adityadroid/bitrise/example/flutter_bitrise_integration_test_automation/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.adityadroid.bitrise.example.flutter_bitrise_integration_test_automation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
